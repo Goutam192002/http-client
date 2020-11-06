624070dacc214b0c19db7c253c86653b344e2e3e
Goutam <goutambseervi@gmail.com>
2020-11-06 11:26:59 +0530

    feature: added post requests

---
+++ test/post.js
@@ -0,0 +1,14 @@
+const expect = require('chai').expect;
+const http = require('../index');
+
+describe('POST http method', () => {
+    it('should make a normal post request with correct content type', async function () {
+        var result = await http.post('http://localhost:3000/post', {
+            contentType: 'application/json',
+            data: {
+                name: 'Goutam'
+            }
+        });
+        expect(result.data).to.equal('Goutam');
+    });
+});
